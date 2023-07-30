1a9c42bbe087d3737313a260fa5d32dcecd19f4d
Yazan Nogura <nogura@outlook.com>
2023-07-30 10:20:38 -0500

    new notes

---
+++ js_basics/operators_arithmetic_practices.js
@@ -0,0 +1,45 @@
+
+// TASK-1
+
+/* 
+Create 2 variables and assign them numbers. 
+Then, find the sum, subtraction, multiplication, division and remainder of these 2 numbers.
+*/
+
+
+/*
+Write a program that calculates and outputs the area and perimeter of a rectangle. 
+Interview Question!
+*/
+
+let a = 15;
+let b = 5;
+
+let area = a * b;
+let perimeter = 2*(a + b); 
+
+console.log(area);
+console.log(perimeter);
+
+
+
+/*
+An annual average salary for an SDET in the Unites States is 120k. 
+Write a program that calculates and prints the monthly and bi-weekly and 
+weekly average amount that an SDET makes in the United States.
+*/
+
+let avgSalary = 120000
+let monthsInYear = 12;
+let weeksInYear = 52;
+
+console.log(avgSalary / monthsInYear);
+console.log(avgSalary / weeksInYear);
+console.log(avgSalary / weeksInYear * 2);
+
+
+
+
+
+
+
--- js_basics/operators_arthimetic.js
+++ js_basics/operators_arthimetic.js
@@ -14,51 +14,19 @@ console.log(num5 - num5); // 0
 console.log(num1 * num2); // 50
 console.log(num2 * num3 * num4 * num5);
 
+console.log(num2 * num3 * num4 * num5); // 210
+
+console.log(num1 / num4); // 5
+
+console.log(num2 % num1); // 5
+console.log(num3 % num4); // 1
+console.log((num5 + num1) % num3); // 6
 
 
-/* 
-Create 2 variables and assign them numbers. 
-Then, find the sum, subtraction, multiplication, division and remainder of these 2 numbers.
-*/
-
-let number1 = 6, number2 = 4;
-
-console.log(number1 + number2);
-console.log(number1 - number2);
-console.log(number1 * number2);
-console.log(number1 / number2);
-console.log(number1 % number2);
-
-
-/*
-Write a program that calculates and outputs the area and perimeter of a rectangle. 
-Interview Question!
-*/
-
-let a = 15;
-let b = 5;
-
-let area = a * b;
-let perimeter = 2*(a + b); 
-
-console.log(area);
-console.log(perimeter);
-
-
-
-/*
-An annual average salary for an SDET in the Unites States is 120k. 
-Write a program that calculates and prints the monthly and bi-weekly and 
-weekly average amount that an SDET makes in the United States.
-*/
-
-let avgSalary = 120000
-let monthsInYear = 12;
-let weeksInYear = 52;
-
-console.log(avgSalary / monthsInYear);
-console.log(avgSalary / weeksInYear);
-console.log(avgSalary / weeksInYear * 2);
+console.log(3 * 5 + 2); // 17
+console.log(5 - 3 + 10 / 2 + 13); // 20
+console.log((5 - 2) * 5 ** 2); // 75
+console.log(2 ** 3 * 8 / 16 * 2); // 8
 
 
 
