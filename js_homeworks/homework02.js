acfada229f726eb7731667753201b1a50ad024e0
Yazan Nogura <nogura@outlook.com>
2023-08-05 10:08:13 -0500

    new notes

--- js_homeworks/homework02.js
+++ js_homeworks/homework02.js
@@ -1,16 +1,35 @@
 // Task-1
-
 console.log("------------Task-1------------");
+
+let str1 = "5", str2 = "2";
+
+console.log(`The sum of ${str1} and ${str2} = ${parseInt(str1) + parseInt(str2)}`);
+console.log(`The product of ${str1} and ${str2} = ${str1 * str2}`);
+console.log(`The division of ${str1} and ${str2} = ${str1 / str2}`);
+console.log(`The subtraction of ${str1} and ${str2} = ${str1 - str2}`);
+console.log(`The remiander of ${str1} and ${str2} = ${str1 % str2}`);
+console.log(`The exponentitation of ${str1} and ${str2} = ${str1 ** str2}`);
 
 // Task-2
 
 console.log("------------Task-2------------");
 
-//Task-3
+// Task-3
 
 console.log("------------Task-3------------");
 
+// Task-4
+
 console.log("------------Task-4------------");
+
+// Task-5
+
 console.log("------------Task-5------------");
+
+// Task-6
+
 console.log("------------Task-6------------");
+
+// Task-7
+
 console.log("------------Task-7------------");
