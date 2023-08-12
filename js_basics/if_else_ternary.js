3fea0a3b5450ae5d2006264f63efd797c367e118
Yazan Nogura <nogura@outlook.com>
2023-08-12 13:00:46 -0500

    new notes

---
+++ js_basics/if_else_ternary.js
@@ -0,0 +1,17 @@
+
+let num = 5;
+
+if (num % 2 === 0) console.log("EVEN");
+else console.log("ODD");
+
+num % 2 === 0 ? console.log("EVEN") : console.log("ODD");
+
+
+console.log("\n---------------------\n");
+
+let number = 25;
+
+number % 10 === 0 ? console.log(true) : console.log(false);
+
+console.log(number % 10 === 0);
+
