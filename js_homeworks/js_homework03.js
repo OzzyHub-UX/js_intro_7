2cca566f9c5fd480fc07da66e97d4714ce11f25f
Yazan Nogura <nogura@outlook.com>
2023-08-13 10:55:12 -0500

    new notes

---
+++ js_homeworks/js_homework03.js
@@ -0,0 +1,45 @@
+// Function for RandomNumbers
+function getRandomNumber(start,end) {
+    return Math.floor(Math.random() * (end - start  + 1) + start);
+}
+
+
+// Task-1
+console.log("\n------------Task01------------\n");
+
+
+let randomNum1 = getRandomNumber(0,100);
+let randomNum2 = getRandomNumber(0,100);
+let randomNum3 = getRandomNumber(0,100);
+let avg3 = Math.round((randomNum1 + randomNum2 + randomNum3) / 3);
+
+avg3 >= 50 ? console.log(true) : console.log(false);
+
+
+// Task-2
+console.log("\n------------Task02------------\n");
+
+let randomNum4 = getRandomNumber(1,3);
+let randomNum5 = getRandomNumber(1,3);
+let randomNum6 = getRandomNumber(1,3);
+
+if (randomNum4 === randomNum5 && randomNum4 === randomNum6 && randomNum5 === randomNum6) console.log("TRIPLE MATCH");
+else if (randomNum4 === randomNum5 || randomNum4 === randomNum6 || randomNum5 === randomNum6 ) console.log("DOUBLE MATCH");
+else console.log("NO MATCH");
+
+
+
+console.log("\n------------Task03------------\n");
+console.log("\n------------Task04------------\n");
+console.log("\n------------Task05------------\n");
+console.log("\n------------Task06------------\n");
+console.log("\n------------Task07------------\n");
+console.log("\n------------Task08------------\n");
+console.log("\n------------Task09------------\n");
+console.log("\n------------Task10------------\n");
+console.log("\n------------Task11------------\n");
+
+let result = 5 === 2 + "3" ? "FOO" : "BAR";
+console.log(result);
+
+console.log(Math.pow(2,3) /4);
