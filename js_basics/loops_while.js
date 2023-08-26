bbfb4cf351c521d82db2b04ee2d919afd6076023
OzzyHub-UX <nogura@outlook.com>
2023-08-26 10:54:32 -0500

    new notes

--- js_basics/loops_while.js
+++ js_basics/loops_while.js
@@ -1,13 +1,16 @@
 
-for(let i = 1; i < 20; i++) {
+for(let i = 1; i < 10; i++) {
     console.log(i);
 }
 
 
 // while version of above
 let num = 1;
+let countE = 0;
 
 while(num < 10){
-    console.log(num);
+    if(num % 2 === 0) countE++;
     num++;
 }
+
+console.log(countE);
