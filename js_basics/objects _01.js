1a5bb06cd0bea595c138bca8c1319dc738503470
OzzyHub-UX <nogura@outlook.com>
2023-08-28 19:01:45 -0500

    new notes

--- js_basics/objects _01.js
+++ js_basics/objects _01.js
@@ -4,4 +4,32 @@ const person = {
     city : 'Chicago'
 };
 
-console.log(person);
+console.log(person); // { fName: 'John', age: 25, city: 'Chicago' }
+
+// adding more properties
+person.lName = 'Doe';
+person['gender'] = 'Male';
+
+console.log(person); // {fName: 'John', age: 25,city: 'Chicago',lName: 'Doe',gender: 'Male'}
+
+let personAge = person.age;
+console.log(personAge); // 25
+
+// Accessing object properties
+console.log(person.fName); // John
+console.log(person['city']); // Chicago
+
+// Updating values for the specified key
+person.fName = 'Jane';
+person['fName'] = 'Jane';
+
+console.log(person.fName); // Jane
+console.log(person.lName); // Smith
+
+// deleting object properties
+delete person.gender;
+delete person.lName;
+delete person['city'];
+
+console.log(person); //{ fName: 'Jane', age: 25 }
+
