4ebbf2ad1ed61f601f9bbab7a7187ea3e4a7cb9a
OzzyHub-UX <nogura@outlook.com>
2023-08-28 19:33:38 -0500

    new notes

--- js_basics/object_02.js
+++ js_basics/object_02.js
@@ -27,3 +27,18 @@ console.log(student.exam_scores);
 console.log(student.hobbies[0]);
 console.log((student.exam_scores.midterm + student.exam_scores.final) / 2);
 
+console.log("-----------------------------");
+
+const book = {
+    name : 'Amok',
+    author : {
+        fName : 'Stefan',
+        lName : 'Zewig',
+    },
+    genre : 'Novella',
+};
+
+console.log(book);
+
+
+console.log("-----------------------------");
