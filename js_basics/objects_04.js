28a3cc3ff390b5042d4b5e31331f8bd0e161572f
OzzyHub-UX <nogura@outlook.com>
2023-08-30 14:30:42 -0500

    new notes

--- js_basics/objects_04.js
+++ js_basics/objects_04.js
@@ -110,3 +110,73 @@ for(const user of users) {
 }
 
 console.log(`The average of all the ages = ${sumOfAges/users.length}`);
+
+
+
+
+
+console.log("\n----------------------------------------------\n");
+
+
+const appleStore = [
+    {
+        productName: "iPhone 14 Pro",
+        quantity: 50,
+        price: 1099.99,
+    },
+    {
+        productName: "MacBook Pro 16-inch",
+        quantity: 30,
+        price: 2399.99,
+    },
+    {
+        productName: "iPad Air",
+        quantity: 75,
+        price: 599.99,
+    },
+    {
+        productName: "Apple Watch Series 7",
+        quantity: 100,
+        price: 399.99,
+    },
+    {
+        productName: "AirPods Pro",
+        quantity: 200,
+        price: 249.99,
+    },
+];
+// Find the most expensive product   -> MacBook Pro 16-inch
+// Find the least expensive product   -> AirPods Pro
+// Find the product with the biggest quantity   -> AirPods Pro
+// Find the product with the smallest quantity  -> MacBook Pro 16-inch
+// Find all the product names   -> [ 'iPhone 14 Pro', 'MacBook Pro 16-inch', 'iPad Air', 'Apple Watch Series 7', 'AirPods Pro' ]
+
+let mostExpensive = {
+    productName : null,
+    price : 0,
+    quantity : 0
+};
+
+let leastExpensiveProduct = appleStore[0];
+
+for(const product of appleStore){
+    if(product.price > mostExpensive.price) mostExpensive = product;
+    if(product.price < leastExpensiveProduct.price) leastExpensiveProduct = product;
+}
+
+
+
+console.log(`The most expensive product is a ${mostExpensive.productName}.`);
+console.log(`The least expensive product is ${leastExpensiveProduct}`);
+//console.log(`The product with the biggest quantity is ${}.`);
+//console.log(`The product with the smallest quantity is ${}.`);
+
+let productNames = [];
+
+for (const product of appleStore){
+    productNames.push(product.productName);
+}
+
+console.log(productNames);
+
+
