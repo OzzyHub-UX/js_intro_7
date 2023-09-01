405e0eb5bd3fb3f03d1a91121019f1d15b5cd051
OzzyHub-UX <nogura@outlook.com>
2023-09-01 06:39:41 -0500

    new notes

--- js_homeworks/homework05.js
+++ js_homeworks/homework05.js
@@ -2,6 +2,7 @@
 //Task01
 console.log("\n-------------------------------------\n");
 
+// For Of Loop
 function countPos(arr1){
 
     let countPos = 0;
@@ -16,3 +17,14 @@ function countPos(arr1){
 console.log(countPos([-45,0,0,34,5,67]));
 console.log(countPos([-23,-4,0,2,5,90,123]));
 console.log(countPos([0,-1,-2,-3]));
+
+//For Loop
+
+function countPos1(arr1) {
+
+    let countPos1
+}
+
+
+//Task02
+console.log("\n-------------------------------------\n");
