1fcff984a42fe50e84eb1f537202826deeaed8f1
OzzyHub-UX <nogura@outlook.com>
2023-09-02 11:39:32 -0500

    new notes

--- js_basics/array-callback-functions-forEach.js
+++ js_basics/array-callback-functions-forEach.js
@@ -20,3 +20,28 @@ const printNameUpperCase = (name) => console.log(name.toUpperCase());
 names.forEach(printNameUpperCase);
 
 
+// Count how many names has 4 letters
+
+
+let count = 0;
+
+names.forEach((name) => name.length === 4 ? count++ : count);
+
+console.log(count);
+
+//Above is not good, we will learn filter() or reduce could be used 
+
+
+console.log("\n");
+
+const numbers = [5, 10, 3, 0, -2];
+
+numbers.forEach((number) => console.log(number));
+
+numbers.forEach((number) => console.log(number * 5));
+
+numbers.forEach((number) =>console.log(number % 2 === 0));
+
+
+const numbersMultipliedBy3 = numbers.map((num) => num * 3);
+console.log(numbersMultipliedBy3);
