6ad21fef2e25fe3d258befab3e2a13b760346360
OzzyHub-UX <nogura@outlook.com>
2023-09-02 10:25:55 -0500

    new notes

--- js_basics/arrow_functions.js
+++ js_basics/arrow_functions.js
@@ -13,3 +13,25 @@ function product1(num1, num2){
 console.log(product1(3, 5));
 console.log(product1(2, 0));
 console.log(product1(4, 3));
+
+
+console.log("|\n|");
+
+// Function expression syntax
+const product2 = function (num1, num2) { return num1 * num2};
+
+console.log(product2(3, 5));
+console.log(product2(2, 0));
+console.log(product2(4, 3));
+
+
+console.log("|\n|");
+
+
+// Arrow Function
+const product3 = (num1, num2) => num1 * num2;
+
+console.log(product3(3, 5));
+console.log(product3(2, 0));
+console.log(product3(4, 3));
+
