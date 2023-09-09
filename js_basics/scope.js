a14f1ccd4d512cd03fdb15cb77c9115dcb7a6c63
OzzyHub-UX <nogura@outlook.com>
2023-09-09 14:20:25 -0500

    new notes

---
+++ js_basics/scope.js
@@ -0,0 +1,16 @@
+
+/**
+ * Global Scope
+ */
+
+let globalVariable = 'This is global variable';
+
+console.log(globalVariable); // 'This is global variable'
+
+// Global variable is accessible inside function
+function someFunction() {
+    console.log(globalVariable); // 'This is global variable'
+}
+console.log(color); 
+
+print
