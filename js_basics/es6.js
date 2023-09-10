c014bb2dc5ae38eaef55e96b0f4cf3913532b5b9
OzzyHub-UX <nogura@outlook.com>
2023-09-10 11:10:07 -0500

    new notes

---
+++ js_basics/es6.js
@@ -0,0 +1,14 @@
+// CONTINUE ES6
+// Enhanced Object Literals
+
+//values coming from somewhere
+const year = 2023;
+const type = 'horror';
+
+// before ES6
+const movie = {
+    year: year,
+    type: type
+};
+
+// after es6 
