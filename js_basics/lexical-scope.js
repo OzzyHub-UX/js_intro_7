8bd736ea8f3afb4983ba6847fe473067437ac5b7
OzzyHub-UX <nogura@outlook.com>
2023-09-12 18:40:10 -0500

    new notes

--- js_basics/lexical-scope.js
+++ js_basics/lexical-scope.js
@@ -1,11 +1,11 @@
-const schoolName = "Tech Global";
+const schoolName1 = "Tech Global";
 
 function schoolProfile() {
     const schoolName = "Global Tech";
     function saySchoolName() {
         const schoolName = "Tech";
         function writeSchoolName() {
-            return schoolName; // "Tech Global"
+            return schoolName; //
         }
        return writeSchoolName();
     }
@@ -15,3 +15,17 @@ function schoolProfile() {
 console.log(schoolProfile());
 // Scope Chaining 
 // writeSchoolName() scope ---> saySchoolName() scope ---> schoolProfile() scope ---> global scope
+
+
+
+// Define a function:
+function getCityName() {
+    const cityName = "Des Plaines";
+    return cityName;
+}
+
+function showAddress() {
+    return "2860 River Rd, " + getCityName();
+}
+
+console.log(showAddress());
