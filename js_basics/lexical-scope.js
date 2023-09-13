1472d47a53de9af5a0ab64267124ce31946cefff
OzzyHub-UX <nogura@outlook.com>
2023-09-13 11:25:35 -0500

    new notes

--- js_basics/lexical-scope.js
+++ js_basics/lexical-scope.js
@@ -33,3 +33,33 @@ console.log(showAddress());
 console.log("foo:", foo);
 console.log("bar", bar);
 var foo = 'foo';
+
+hello();
+function hello() {
+    bye()
+    console.log('Hello');
+    bye()
+    function bye() {
+        console.log('Bye')
+    }
+}
+// bind()
+
+let nameObj = {
+    name: "Tony"
+}
+
+
+let PrintName = {
+    name: "steve",
+    sayHi: function () {
+        // Here "this" points to nameObj
+        console.log(this.name);
+    }
+};
+
+PrintName.sayHi();
+
+const sayHiBind = PrintName.sayHi.bind(nameObj);
+console.log(typeof(sayHiBind));
+sayHiBind();
