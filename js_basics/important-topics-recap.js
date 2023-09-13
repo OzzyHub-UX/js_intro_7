a239c8c5d89475c3ac853487c925b5c2c95e03d8
OzzyHub-UX <nogura@outlook.com>
2023-09-13 18:34:31 -0500

    new notes

--- js_basics/important-topics-recap.js
+++ js_basics/important-topics-recap.js
@@ -1,8 +1,43 @@
 // Keyword "this"
 
 function printName(name) {
-    console.log(this);
+    console.log(this); // window or object
     console.log(name);
 };
 
-printName();
+printName('Ulan');
+
+// Implicit Binding
+
+const person = {
+    name: 'Alan',
+    age: 29,
+    printName() {
+        console.log(this.name); // value of this?
+    },
+    mother: {
+        hobbies: ['running', 'baking'],
+        name: 'Jane',
+        printName() {
+            console.log(this.name);
+            console.log(this);
+        }
+    }
+}
+
+person.printName(); // Alan, this = person object
+person.mother.printName(); // Jane, this = mother object 
+
+// this (person) => this (window)
+
+// Function is not inside the object 
+const person1 = {
+    name: 'Alan',
+    age: 29,
+};
+
+function printName1() {
+    console.log(this.name);
+};
+
+printName1.call(person1);
