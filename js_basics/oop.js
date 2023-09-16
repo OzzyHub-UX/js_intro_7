414f764eeb979b235c03f52c9d097b11e5a9b729
OzzyHub-UX <nogura@outlook.com>
2023-09-16 11:16:00 -0500

    new notes

--- js_basics/oop.js
+++ js_basics/oop.js
@@ -21,3 +21,35 @@ const alex = createPerson('Alex');
 const joe = createPerson('Joe');
 
 yulia.greet();
+
+
+// Prototypical Chain
+
+// Create a new date object
+const date = new Date('December 17, 1995 03:24:00');
+console.log(date);
+console.log(typeof date); // object
+
+// These methods coming from Date
+const fullYear = date.getFullYear();
+console.log(fullYear);
+console.log(typeof fullYear);
+
+// These methods coming from object 
+console.log(date.toString());
+console.log(typeof date.toString());
+
+// date ->  Date -> Object -> null
+
+
+// Constructor function
+const cat = function(name, breed) {
+    this.name = name;
+    this.breed = breed;
+}
+
+cat.prototype.getDetails = function() {
+    console.log(`${this.name} is a ${this.breed}`);
+};
+
+
