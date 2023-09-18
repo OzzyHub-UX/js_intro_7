2547c512e74c041a55ecc66bfdc1fae809d03459
OzzyHub-UX <nogura@outlook.com>
2023-09-17 21:51:08 -0500

    new notes

--- js_homeworks/homework07.js
+++ js_homeworks/homework07.js
@@ -1,7 +1,31 @@
 // Task01
-console.log();
+console.log("--------------------TASK-01--------------------");
+
+const hasUpperCase = (str) => {
+
+    for(let i = 0; i <= str.length - 1; i++) {
+        if(str[i].charCodeAt(i) > 65 && str[i].charCodeAt(i) < 90) return true;
+    }
+    return false;
+}
+
+console.log(hasUpperCase("javascript"));
+console.log(hasUpperCase("John"));
+console.log(hasUpperCase("$125.0"));
+console.log(hasUpperCase(""));
+
 
 //Task02
+console.log("--------------------TASK-02--------------------");
+
+const noDigit = (str) => {
+
+    for(let i = 0; i <= str.length - 1; i++) {
+
+        
+
+    }
+}
 
 //Task03
 
