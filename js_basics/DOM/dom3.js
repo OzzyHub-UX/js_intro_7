43d33c33b6c82e55b4190a6bd733956169d51ec3
OzzyHub-UX <nogura@outlook.com>
2023-09-20 18:17:44 -0500

    new notes

--- js_basics/DOM/dom3.js
+++ js_basics/DOM/dom3.js
@@ -1,3 +1,8 @@
 // Import all elements
 
 const ulElement = document.getElementById('parent');
+
+
+// Get all the child nodes of parentElement
+const childLi = parentElement.childNodes;
+console.log(childLi);
