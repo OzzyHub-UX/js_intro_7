7e34d38ec91305f2ab54723566ed35209cbe8be6
OzzyHub-UX <nogura@outlook.com>
2023-09-19 20:17:04 -0500

    new notes

--- js_basics/DOM/dom.js
+++ js_basics/DOM/dom.js
@@ -32,9 +32,56 @@ console.log("height", innerHeight);
 
 // localStorage
 console.log(window.localStorage.length); // 0 empty
-// set item in local storage
+
+// Set item in local storage
 window.localStorage.setItem('name', 'John')
 window.localStorage.setItem('issLoggedIn', false)
 window.localStorage.setItem('userId', 1234)
-window.localStorage.setItem('userInfo', {name: 'John', email: 'johndoe@gmail.com'})
+window.localStorage.setItem('userInfo', JSON.stringify({ name: 'John', email: 'johndoe@gmail.com' }));
 
+// Get item from localStorage 
+console.log(window.localStorage.getItem('name', 'John'))
+console.log(window.localStorage.getItem('issLoggedIn', false))
+console.log(window.localStorage.getItem('userId', 1234))
+console.log(window.localStorage.getItem('userInfo'));
+
+
+
+
+
+
+
+let nameUser = window.prompt('Hello! What is your name?')
+console.log(nameUser);
+
+if (nameUser === null || nameUser === '' || nameUser === undefined) {
+    window.alert('You didnt provide your name. Bye Bye!');
+}
+else {
+window.alert('Hello ' + nameUser + '!')
+}
+
+let userContinueConfirm = window.confirm('Would you like to continue?');
+console.log(userContinueConfirm);
+
+if (userContinueConfirm === true) {
+    window.alert('You have chose to continue!')
+    console.log(window.innerWidth);
+    console.log(window.innerHeight);
+
+
+const doesUserWantsRedirect = window.confirm('Would you like to go to a different page or reload the page?')
+if(doesUserWantsRedirect) {
+    setTimeout(() => {
+        window.open('https://google.com');
+    }, 10000)
+}
+else {
+    setTimeout(() => {
+        window.location.reload();
+    }, 10000);
+}
+}
+else {
+    window.alert('You canceled the action!')
+}
