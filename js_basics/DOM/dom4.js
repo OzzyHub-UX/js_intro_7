02365adf7f13d2e9be104302778a33bafc574296
OzzyHub-UX <nogura@outlook.com>
2023-09-23 10:43:37 -0500

    new notes

--- js_basics/DOM/dom4.js
+++ js_basics/DOM/dom4.js
@@ -14,3 +14,26 @@ const dblclick = document.getElementById('dblClickBtn');
 dblclick.addEventListener('dblclick', (event) => {
     console.log('You double clicked =>', event.target);
 })
+
+
+// mouse down
+const mousedownBtn = document.getElementById('mouseDownBtn');
+
+mousedownBtn.addEventListener('mousedown', (event) => {
+console.log('You pressed down => ', event.target)
+});
+
+
+// mouse up
+const mouseUpBtn = document.getElementById('mouseUpBtn');
+
+mouseUpBtn.addEventListener('mouseup', (event) => {
+    console.log('You pressed down => ', event.target)
+    });
+
+// mousemove
+const mouseMoveBtn = document.getElementById('mouseMoveBtn');
+
+mouseMoveBtn.addEventListener('mousemove', (event) => {
+    console.log('You moving inside botton => ', event.target);
+});
