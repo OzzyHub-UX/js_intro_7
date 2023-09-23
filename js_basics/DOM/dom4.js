69c6ea4c3bfe2a7b0533ea8554979c570f33ead0
OzzyHub-UX <nogura@outlook.com>
2023-09-23 10:33:25 -0500

    new notes

--- js_basics/DOM/dom4.js
+++ js_basics/DOM/dom4.js
@@ -5,6 +5,12 @@
 const clickBtn = document.getElementById('clickBtn');
 
 clickBtn.addEventListener('click', (event) => {
-    console.log(event);
-    console.log('Button clicked!');
+    console.log('You clicked! =>', event.target);
+})
+
+// dbclick
+const dblclick = document.getElementById('dblClickBtn');
+
+dblclick.addEventListener('dblclick', (event) => {
+    console.log('You double clicked =>', event.target);
 })
