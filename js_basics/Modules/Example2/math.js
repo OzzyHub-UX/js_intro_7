a6fba69b7b2f15e7935c7f4403b3b03d0a4eff7b
OzzyHub-UX <nogura@outlook.com>
2023-09-24 11:16:08 -0500

    new notes

--- js_basics/Modules/Example2/math.js
+++ js_basics/Modules/Example2/math.js
@@ -2,4 +2,15 @@
 // Named export 
 export const add = (num1, num2) => {
     return num1 + num2;
-}
+};
+
+
+export const subtract = (num1, num2) => {
+    return num1 - num2;
+};
+
+// Different way of exporting named functions
+export {
+    add,
+    subtract
+};
