3a347c1bfa09929c0143aef50a25b0f07c9f8fa3
OzzyHub-UX <nogura@outlook.com>
2023-09-26 12:14:28 -0500

    new notes

--- js_basics/array-callback-functions-forEach.js
+++ js_basics/array-callback-functions-forEach.js
@@ -9,3 +9,24 @@ names.forEach((name) => console.log(name[0]));
 // Output each name uppercased
 names.forEach((name) => console.log(name.toUpperCase()));
 
+// Count how many names has 4 letters  -> 3
+
+let count = 0;
+
+const has4 = (name) => name.length === 4;
+names.forEach((name) => name.length === 4 ? count++ : count);
+
+console.log(count); // 3
+
+// Above is not good, we will learn filter() or reduce() could be used.
+
+const numbers = [5, 10, 3, 0, -2];
+
+// Print each number 
+numbers.forEach((number) => console.log(number));
+
+// Print each number multiplied by 5
+numbers.forEach((number) => console.log(number * 5));
+
+// Print true for even numbers or false for odd numbers.
+numbers.forEach((number) => console.log(number % 2 === 0 ? true : false));
