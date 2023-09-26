2b854e93c7ff612a8da64678e3429a2bfcd8e678
OzzyHub-UX <nogura@outlook.com>
2023-09-26 17:37:02 -0500

    new notes

---
+++ js_basics/array-callback-functions-find.js
@@ -0,0 +1,13 @@
+const numbers = [0, 10, -4, 5, 2, -3];
+
+// First positive               -> 10
+// First negative               -> -4
+// First even                   -> 0
+// First odd                    -> 5
+// First number more than 100   -> undefined
+
+// The index of first positive              -> 1
+// The index of first negative              -> 2
+// The index of first even                  -> 0
+// The index of first odd                   -> 3
+// The index of first number more than 100  -> -1
