9428965d652d805b6db86cd383149294957b9b15
OzzyHub-UX <nogura@outlook.com>
2023-10-01 14:54:43 -0500

    new notes

--- js_homeworks/homework.09.js
+++ js_homeworks/homework.09.js
@@ -24,16 +24,54 @@ console.log("\n---------TASK02---------\n");
 const fizzBuzz2 = (num) => {
     const newArr = [];
 
-    for(let i = 0; i <= num; i++) {
-        if(i % 15 === 0) newArr.push('FizzBuzz')
-        if(i % 3 === 0) newArr.push('Fizz')
-        if(i % 5 === 0) newArr.push('Buzz')
+    for(let i = 1; i <= num; i++) {
+        if(i % 15 === 0) {
+            newArr.push('FizzBuzz')
+            continue;
+        }
+        if(i % 3 === 0) {
+            newArr.push('Fizz')
+            continue;
+        }
+        if(i % 5 === 0) {
+            newArr.push('Buzz')
+            continue;
+        }
         else newArr.push(i)
     }
     return newArr;
 };
 
-fizzBuzz2(3);
+console.log(fizzBuzz2(3));
+console.log(fizzBuzz2(5));
+console.log(fizzBuzz2(10));
+console.log(fizzBuzz2(15));
+console.log(fizzBuzz2(2));
+
+// Task03
+console.log("\n---------TASK03---------\n");
+
+const findSumNumbers = (str) => {
+    let sum = 0;
+
+    for(const i of str) {
+        if('0123456789'.includes(str)) sum += parseInt(str);
+    }
+    return sum
+};
 
 
+console.log(findSumNumbers("abc$"));
+console.log(findSumNumbers("a1b4c 6#"));
+console.log(findSumNumbers("ab110c045d"));
+console.log(findSumNumbers("525"));
+console.log(findSumNumbers("3 for 10 dollars"));
 
+// Task09
+console.log("\n---------TASK09---------\n");
+
+const isPowerOf3 = (number) => (Math.pow(number, 3) % 3 === 0); 
+
+console.log(isPowerOf3(15));
+
+console.log();
