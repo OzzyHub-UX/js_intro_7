af01271cd68a508b000a278fd0a7929d72d30b8e
OzzyHub-UX <nogura@outlook.com>
2023-10-09 23:48:13 -0500

    new notes

--- js_homeworks/homework.09.js
+++ js_homeworks/homework.09.js
@@ -130,6 +130,7 @@ const countOccurrencesOfCharacters = (str) => {
 }
 
 
+
 console.log(countOccurrencesOfCharacters(""));
 console.log(countOccurrencesOfCharacters("abc"));
 console.log(countOccurrencesOfCharacters("abbcca"));
