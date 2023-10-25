40cca81b7598676e745835911346bef2ef92356d
OzzyHub-UX <nogura@outlook.com>
2023-10-25 13:34:39 -0500

    new notes

--- js_homeworks/homework10.js
+++ js_homeworks/homework10.js
@@ -1,3 +1,12 @@
 // Task01
 
-const calculateTotalPrice1 = {apple}
+const shoppingItems = {
+    apple: 2.00,
+    orange: 3.29,
+    mango: 4.99,
+    pineapple: 5.25,
+};
+
+console.log(shoppingItems);
+
+
