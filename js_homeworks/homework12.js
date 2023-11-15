a8d9434c6763a554c09109e1855c27d365b642e8
OzzyHub-UX <nogura@outlook.com>
2023-11-15 15:16:06 -0600

    new notes

--- js_homeworks/homework12.js
+++ js_homeworks/homework12.js
@@ -41,21 +41,31 @@ console.log("\n--------------------\n");
 
 /*
 Write a function named decimal2() which takes an array of 
-numbers as an argument and returns the array with the 
+numbers as an argument and => returns the array with the 
 same numbers rounded up or down and represented with only two decimals.
 */
 
-const decimal2 = (arr) => {
+const decimal2 = (arr) =>{
 
-    const withoutWholeNumbers = arr.filter((number => number % 1 !== 0 ))
-
-    for(const withoutWholeNumbers of number) {
-
+    const roundedNumbers = [];
+  
+    for (const number of arr) {
+      const roundedNumber = Number(number.toFixed(2));
+        roundedNumbers.push(roundedNumber);
     }
+  
+    return roundedNumbers;
 
-}
+  }
 
-console.log(decimal2());
+
+  
+  console.log(decimal2([94.356, 8.9752]));
+  console.log(decimal2([76.62, 128.4, 84]));
+  console.log(decimal2([20987, 3.53245, 12.345, 32.9]));
+  console.log(decimal2([]));
+  console.log(decimal2([4.35623, 8.9742]));
+  
 
 console.log("\n--------------------\n");
 
@@ -70,7 +80,28 @@ power of 1 equals the number itself.
 
 const myPow = (num1, num2) => {
 
-}
+    let result = num1;
+
+    if (num2 === 0) {
+      return 1;
+    }
+  
+    for (let i = 1; i < Math.abs(num2); i++) {
+      result *= num1;
+    }
+      if (num2 < 0) {
+      result = 1 / result;
+    }
+    return result;
+  }
+
+  console.log(myPow(3, 3));
+  console.log(myPow(10, 1));
+  console.log(myPow(100, 0));
+  console.log(myPow(1, 1));
+  console.log(myPow(4, 2));
+  console.log(myPow(0, 0));
+  console.log(myPow(5, 3));
 
 console.log("\n--------------------\n");
 
@@ -84,5 +115,28 @@ return empty string. NOTE: If the string consists of multiple
 words having the longest word, then return the first occurrence.
 */
 
+const findLongestWord = (str) => {
+    if (str.trim() === "") {
+      return "";
+    }
 
+      const words = str.split(" ");
+      let maxLength = 0;
+      let longestWord = "";
+  
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].length > maxLength) {
+        maxLength = words[i].length;
+        longestWord = words[i];
+      }
+    }
+  
+    return longestWord;
+  }
+
+  console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+  console.log(findLongestWord("This is a sample string for testing"));
+  console.log(findLongestWord("One two ten"));
+  console.log(findLongestWord(""));
+  
 console.log("\n--------------------\n");
